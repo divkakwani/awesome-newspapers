260f6b53f0f07be00f35a940b02422004d251966
Divyanshu Kakwani <divkakwani@gmail.com>
2021-03-20 10:34:21 +0530

    update w3scraper.py

--- w3scraper.py
+++ w3scraper.py
@@ -17,9 +17,9 @@ import sys
 import tldextract
 
 
-
 cc = coco.CountryConverter()
 headers = ua = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}
+
 
 lang2country = {}
 for item in available_languages():
